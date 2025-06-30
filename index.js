a526aedb5f641604f2f5d895a77ab000cf4f22e2
Usamagrowth <usamaomodara@gmail.com>
2025-06-30 20:16:25 +0100

    calculator

--- index.js
+++ index.js
@@ -1,21 +1,77 @@
 const display = document.querySelector("#display");
 
-function appendToDisplay(input){
-    if (input === '%') {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-    display.value = `${eval(display.value)} / 100`;
-         } else {
-       display.value += input; 
-    }
+// window.addEventListener("keyup", (e) => {
+//   console.log(e);
+//   if (e.key === "Escape") {
+//     clearDisplay();
+//     return;
+//   }
+
+//   if (e.key >= 0 && e.key <= 9) {
+//     appendToDisplay(e.key);
+//     return;
+//   }
+
+//   if (
+//     e.key === "-" ||
+//     e.key === "/" ||
+//     e.key === "*" ||
+//     e.key === "."
+//   ) {
+//     appendToDisplay(e.key);
+//     return;
+//   }
+    
+//     if (
+//     e.key === "+" 
+//   ) {
+//     appendToDisplay(e.key);
+//     return;
+//   }
+
+//   if ((e.key = "=")) {
+//     calculate(e.key);
+//     return;
+//   }
+//   alert(e.key);
+// });
+
+function appendToDisplay(input) {
+ 
+    display.value += input;
+  }
+
+function operatorDisplay(input) {
+    display.value += input;
 }
 
-function clearDisplay(){
- display.value = "";
+function clearDisplay() {
+  display.value = "";
 }
 
-function calculate(){
-    try {
-      display.value = eval(display.value)   
-    } catch (error) {
-        display.value = "error" 
+function calculate(symb) {
+    let disp = display.value
+    disp = disp.split("")
+    console.log(disp)
+    if(disp.includes("÷")) {
+        disp[disp.indexOf("÷")] = "/"
     }
+    disp = disp.join("") 
+  try {
+    if (symb === "%") {
+      display.value = eval(eval(disp) / 100);
+      return;
+    }
+    display.value = eval(disp);
+  } catch (error) {
+    display.value = "error";
+  }
+}
+
+function backSpace() {
+  const disp = display.value
+    .split("")
+    .slice(0, display.value.length - 1)
+    .join("");
+  display.value = disp;
 }
